fda00d8ac2776f08ec71ad4a68b422ff35c30aff
elys33ve <elys33ve@gmail.com>
2022-07-28 12:08:06 -0400

    build_time_string.c
    for testing the stuff

---
+++ C/build_time_string.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// will use __DATE__ and __TIME__ preprocessor directives but manipulate the strings to be yyyy:mm:dd,hh:mm:ss format
+
+
+int main (){
+    char months[12][4] = {"jan\0","feb\0","mar\0","apr\0","may\0","jun\0","jul\0","aug\0","sep\0","oct\0","nov\0","dec\0"};
+
+    char *date = "Jul 16 2022\0";
+    char time_[] = "20:12:42";
+    char build_time[20] = "";
+
+    char temp[3] = "";
+    int i;
+    
+    char year[5] = "";          // delete these, add straight to build_time
+    char month[4] = "";
+    char day[3] = "";
+
+//      get date
+    strncat(year, date+7, 5);       // year
+
+    strncat(month, date, 3);        // month
+    month[0] = tolower(month[0]);
+    for (i=0;i<12;i++){
+        strncpy(temp, &months[i][0], 3);
+        if (strcmp(temp, month) == 0){
+            if (i<9){
+                sprintf(month, "0%d", i+1);
+            }
+            else {
+                sprintf(month, "%d", i+1);
+            }
+            break;
+        }
+    }
+
+    strncat(day, date+4, 2);        // day
+
+    
+    ////// for test
+    printf("date: %s\n", date);
+    printf("time: %s\n", time_);
+    printf("year: %s\n", year);
+    printf("month: %s\n", month);
+    printf("day: %s\n", day);
+
+//      format and print
+    strcat(build_time, year);
+    build_time[4] = ':';
+    strcat(build_time, month);
+    build_time[7] = ':';
+    strcat(build_time, day);
+    build_time[10] = ',';
+    strcat(build_time, time_);
+
+    printf("%s\n", build_time);
+
+    return 0;
+}
