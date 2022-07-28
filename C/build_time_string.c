ae44e1e417b4de829459bf691447605606f8d09d
elys33ve <elys33ve@gmail.com>
2022-07-28 15:41:45 -0400

    build_time_string.c
    added separate function for format_date_time

--- C/build_time_string.c
+++ C/build_time_string.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
 // will use __DATE__ and __TIME__ preprocessor directives but manipulate the strings to be yyyy:mm:dd:hh:mm:ss format
+// should be a function that returns formated string
 
-int main (){
+
+char * format_date_time(char *date, char *time, char str[]){ 
+    //char *date; char *time;
     char months[12][4] = {"jan\0","feb\0","mar\0","apr\0","may\0","jun\0","jul\0","aug\0","sep\0","oct\0","nov\0","dec\0"};
-
-    char *date = "Jul 16 2022\0";
-    char *time = "20:12:42";
-    char str[20];
+    //char str[20];
 
     char temp[3];
     int i;
@@ -24,8 +25,6 @@ int main (){
     strncpy(month, date, 3);        // month
     month[3] = 0;
     month[0] = tolower(month[0]);
-
-
     for (i=0;i<12;i++){
         strncpy(temp, months[i], 4);
         if (strcmp(temp, month) == 0){
@@ -42,20 +41,24 @@ int main (){
     strncpy(day, date+4, 2);        // day
     day[2] = 0;
 
-    
-    printf("date: %s\n", date);
-    printf("time: %s\n", time);
-    printf("year: %s\n", year);
-    printf("month: %s\n", month);
-    printf("day: %s\n", day);
-
 //      format and print
     strcpy(str, year); str[4] = ':';
-    strcat(str, month); str[8] = ':';
-    strcat(str, day); str[11] = ':';
+    strcat(str, month); str[7] = ':';
+    strcat(str, day); str[10] = ':';
     strcat(str, time);
 
-    printf("%s\n", str);
+    return str;
+}
+ 
+
+int main (){
+    char *date = "Sep 01 9876";
+    char *time = "12:12:32";
+    char *build_time = malloc(20);
+
+    build_time = format_date_time(date, time, build_time);
+
+    printf("%s\n", build_time);
 
     return 0;
 }
