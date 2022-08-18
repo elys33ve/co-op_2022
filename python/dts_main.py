2269cea578c8a79c675613336af3c13095caf942
elys33ve <elys33ve@gmail.com>
2022-08-18 16:47:41 -0400

    update and changed name

---
+++ python/dts_main.py
@@ -0,0 +1,37 @@
+# temporary (?) file used for testing things for the dts file search and webserver stuff
+# drivers: channel, clocktrack, dma_central, tmoip_system
+
+from dts_search import *            # file with search, format, and display functions for dt.dts file
+from dts_webserver import HTML_FILE, dts_webserver
+
+
+###################################################
+
+
+if __name__ == "__main__":
+
+    ### get dictionaries of info
+    channel = get_things("channel")
+    clocktrack = get_things("clocktrack")
+    dma_central = get_things("dma_central")
+    tmoip_system = get_things("tmoip_system")
+
+    ### print things to terminal
+    #show_things(channel)
+    #show_things(clocktrack)
+    #show_things(dma_central)
+    #show_things(tmoip_system)
+
+
+    
+
+
+
+
+
+
+
+
+    ### start webserver
+    #dts_webserver()
+    
