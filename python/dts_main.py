85ca65169db30ef2d5fb8a5ae6faeecb82bf8114
elys33ve <elys33ve@gmail.com>
2022-08-26 11:22:35 -0400

    finished for now

--- python/dts_main.py
+++ python/dts_main.py
@@ -3,25 +3,19 @@
 
 from dts_search import *            # file with search, format, and display functions for dt.dts file
 from dts_webshow import app, PORT, HOST
+import dts_webshow as s
 
 ###################################################
 
 
 if __name__ == "__main__":
 
-    ### get dictionaries of info
-    channel = get_things("channel")
-    clocktrack = get_things("clocktrack")
-    dma_central = get_things("dma_central")
-    tmoip_system = get_things("tmoip_system")
-
     ### print things to terminal
-    #show_things(channel)
-    #show_things(clocktrack)
-    #show_things(dma_central)
-    #show_things(tmoip_system)
+    #show_things(s.channel)
+    #show_things(s.clocktrack)
+    #show_things(s.dma_central)
+    #show_things(s.tmoip_system)
 
 
     ### start webserver
     app.run(debug=True, port=PORT, host=HOST)
-
