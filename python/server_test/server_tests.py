a99b098917fa96d0851945afd9b0fa7751fcc776
elys33ve <elys33ve@gmail.com>
2022-09-29 16:54:45 -0400

    stuff works

--- python/server_test/server_tests.py
+++ python/server_test/server_tests.py
@@ -9,34 +9,124 @@ this script is function defs for the command line stuff
 """
 
 from server_test_info import *
+from time import sleep
 import iperf3, paramiko, os
 
+
+#################################
+
+"""
+iperf3 -B IPADDRESS -s
+iperf3 -u -B IPCLIENT -c IPSERVER -b 400m -t 600 -l 1412 -Z
+
+ssh admin@powerstrip -oHostKeyAlgorithms=+ssh-rsa -oPubkeyAcceptedAlgorithms=+ssh-rsa 
+uom set "relay/outlets/1/state" "false"
+uom set "relay/outlets/1/state" "true"
+"""
+
+#################################
+
+ifconfig_results = {}
+#################################
+
+def read_file (file):                   # print file contents by line to terminal (for test)
+    o = open(file, 'r')
+    for line in o:
+        print(line)
+
+def clear_file (file):                  # clear defult results file
+    w = open(file, 'w')
+    w.close()
+
 #################################
 
 
+### gets errors number results from ifconfig output and adds to dict
+def ifconfig_parse (ifconfig, num='x'):
+    errs = []
 
-#################################
+    while "RX errors" in ifconfig:
+        idx = ifconfig.index("RX errors") + 10              # idx first error result
+        ifconfig = ifconfig.replace(ifconfig[0:idx], '')        # replace everything before
+
+        idx = ifconfig.index(" ")                       # idx next space
+        errs.append(ifconfig[0:idx])                        # add everything before to list (errors number)
+
+    if num != 'x':
+        ifconfig_results.update({f"test {num}":errs})
+    else:
+        return errs
+
+
+
+
+### append to txt file
+def append_file (result_list, new_file=False, filename='test_results.txt'):
+    a = open(filename, 'a')
+
+    ### ADD LATER ALSKDJFL;SAKDJF;LSAKDJ ---- if new_file == True, and defult filename exists, create new file
+    ### with defult name +1 until DNE
+
+    for i in range(len(result_list)):           # write results to file
+        a.write(f"{result_list[i]}\n")
+
+    a.close()
+
+
+### print ressults
+def print_results ():
+    for i in range(len(ifconfig_results)):
+        print(ifconfig_results[f'test i'])
+
+
+
+##########################################################################################3
+
+
 
 
 ### ssh
-def ssh (command, host=board_ip, username=board_user, password=board_pass):
+def ssh (command, host=board_ip, username=board_user, password=board_pass, readout=True):
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, username=username, password=password)
     _stdin, _stdout,_stderr = client.exec_command(command)
 
-    output = _stdout.read().decode()
+    if readout == True:
+        print("_stderr = {}\n_stdout = {}".format(_stderr, _stdout))
+        #output = _stdout.read().decode()
 
     client.close()
-    return output
+    return _stdout
+
+
+### run server test on board
+def run_server (server_ip=server_ip, readout=True):
+    server_start = f'iperf3 -B {server_ip} -s'
+
+    if board_ip == server_ip or board_data == server_ip:
+        ssh(server_start, readout=readout)
+    else:
+        x = os.popen(server_start).read()
+
+
+
+### run client test on pc (and wait for test to finish)
+def run_client (client_ip=client_ip, server_ip=server_ip, seconds=seconds, mbps=mbps, readout=True):
+    client_start = f'iperf3 -u -B {client_ip} -c {server_ip} -b {mbps}m -t {seconds} -l 1412 -Z'
+
+    print(f"{board_ip}, {server_ip}, {board_data}, {server_ip}")
+    if board_ip == client_ip or board_data == client_ip:
+        ssh(client_start, readout=readout)
+    else:
+        x = os.popen(client_start).read()
 
 
 
 ### turn on power (and wait for board to boot)
-def power_cycle (do=2):                             # 0=off, 1=on, 2=reset (off/on)
-    OFF = 'uom set "relay/outlets/1/state" "false"'
-    ON = 'uom set "relay/outlets/1/state" "true"'
-    
+def power_cycle (do=2, outlet=outlet):                             # 0=off, 1=on, 2=reset (off/on)
+    OFF = f'uom set "relay/outlets/{outlet}/state" "false"'
+    ON = f'uom set "relay/outlets/{outlet}/state" "true"'
     if do == 0:         # turn off
         ssh(OFF, power_ip, power_user, power_pass)
     elif do == 1:       # turn on
@@ -45,24 +135,11 @@ def power_cycle (do=2):                             # 0=off, 1=on, 2=reset (off/
         ssh(OFF, power_ip, power_user, power_pass)
         ssh(ON, power_ip, power_user, power_pass)    
 
-### run server test on board
-def run_server ():
-    server = iperf3.Server()
-    server.bind_address = server_ip
-    server.port = port
-    server.verbose = False
-
-    while True:
-        server.run()
 
 
-### run client test on pc (and wait for test to finish)
-def run_client ():
-    client = iperf3.Client()
-    client.server_hostname = client_ip
-    client.port = port
-    client.json_output = False
-    result = client.run()
+### wait for board to boot up after power on
+def wait_start ():
+    sleep(10)
 
 
 ### run ifconfig on board (and get temp txt file of results)
