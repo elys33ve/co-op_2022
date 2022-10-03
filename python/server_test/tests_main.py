0683f1ec23f12ca1ad479b0f6b8a153ad6d8c107
elys33ve <elys33ve@gmail.com>
2022-10-03 17:20:07 -0400

    yea this doesnt do much anymore, here for convenience

--- python/server_test/tests_main.py
+++ python/server_test/tests_main.py
@@ -8,11 +8,10 @@
 #   - wait for client to finish test
 #   - run ifconfig on board -- (output results to temp txt file)
 #   - get errors from ifconfig output
-#   - record errors from each trial in separate txt file
 
-from ifconfig_parse import *        # file and parsing stuff
 from server_tests import *          # command line stuff
-from time import sleep
+
+
 
 
 #################################
@@ -22,21 +21,5 @@ from time import sleep
 
 if __name__ == "__main__":
 
-    #power_cycle()      # turn on
-
-    for i in range(num_tests):
-        print(f"\nrunning test {i+1} of {num_tests}")
-        print(f"server = {server_ip}\tclient = {client_ip}")
-        print(f"test time = {seconds} seconds\n")
-
-        run_server()
-        run_client(seconds=60)
-
-        print(f"\ntest {i+1} complete.\n")
-
-    
-
-    
-    #x = ssh("ifconfig")
-
+    test1()    
     
