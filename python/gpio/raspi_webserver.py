8b69b2299d6e1c9d1c69b1f1320592f9ddcdadb4
elys33ve <elys33ve@gmail.com>
2022-10-21 16:13:19 -0400

    added input/output, fixed issues

--- python/gpio/raspi_webserver.py
+++ python/gpio/raspi_webserver.py
@@ -6,142 +6,43 @@
 # http://127.0.0.1:8000/
 
 from dash import Dash, html, dcc, Input, Output
+from raspi_lists import *
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """     REPLACE 'ospinout' AND 'ospininfo' WITH OS COMMANDS ONCE ON RASPI   """
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-from things import pinout as ospinout
-from things import pininfo as ospininfo
-
 ############################################    ---     CONSTANTS
-ON = 'on'
-OFF = 'off'
+ON = 'dh'
+OFF = 'dl'
 
 INPUT = 'ip'
 OUTPUT = 'op'
-
-### lists
-GPIO_PINS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27']
-NOT_GPIO_PINS = ['3V3', 'GND', '5V', '--']
 
 ### webserver
 PORT = 8000
 HOST = '127.0.0.1'              # dash defult
 
 app = Dash(__name__)
-############################################    ---     STRING MANIPULATION / LISTS
-
-### GET STRING OF PIN NUMBERS (FROM PINOUT)
-def pinout_str ():
-    idx1 = ospinout.index('J8:')
-    idx2 = ospinout.index('POE:')
-
-    pinout = ospinout[idx1:idx2-1]
-
-    return pinout
-
-
-### GET INFO ABOUT PIN (LVL,FUNC)
-def pininfo_get (pin):                           # get level, fsel, and function of pin
-    pininfo = ospininfo           # f"raspi-gpio get {pin}"
-    lvl = pininfo[pininfo.index('level=') + 6]
-    func = pininfo[pininfo.index('func=') + 5]
-
-    if lvl == '0':   
-        lvl = 'dl'
-    elif lvl == '1':
-        lvl = 'dh'
-
-    if func == 'O':
-        func = OUTPUT
-    elif func == 'I':
-        func = INPUT
-
-    return lvl, func
-
-
-### CREATE LIST OF PIN AND GPIO NUMBERS (together)
-def pinout_list ():
-    pinout = pinout_str().replace('J8:', '')
-    pinout_keys = []
-    tmp_list = [] 
-    tmp = ''            # current word
-
-    for i in range(len(pinout)):
-        if pinout[i].isspace() == False:        # add letters/numbers until space
-            tmp += pinout[i]
-        elif pinout[i].isspace() == True and tmp != '':     # separate the gpio from (#) i guess
-            pinout_keys.append(tmp)
-            tmp = ''
-
-    while len(pinout_keys) > 2:         # put the gpio/pin with (#) together and add to list
-        pin = pinout_keys[0]+' '+pinout_keys[1]
-        tmp_list.append(pin.strip())
-        pinout_keys.remove(pinout_keys[0])
-        pinout_keys.remove(pinout_keys[0])
-    tmp_list.append(pinout_keys[0]+' '+pinout_keys[1])
-
-    pinout_keys = tmp_list
-
-    return pinout_keys
-
-
-### GET GPIO NUMS FOR PINS
-def gpio_nums ():
-    pinout_keys = pinout_list()
-    gpio_list = []
-
-    for i in range(len(pinout_keys)):
-        if "GPIO" in pinout_keys[i]:
-            gpio = pinout_keys[i].replace(f"({i+1})", "").replace("GPIO","").strip()
-        elif "3V" in pinout_keys[i]:
-            gpio = "3V3"
-        elif "5V" in pinout_keys[i]:
-            gpio = "5V"
-        elif "GND" in pinout_keys[i]:
-            gpio = "GND"
-        else:
-            gpio = "--"
-        gpio_list.append(gpio)
-
-    return gpio_list
-    
+############################################
 
 
 
-############################################    ---     LISTS
-keys = pinout_list()
 
-### CREATE TWO LISTS OF PIN NUMBERS (RIGHT AND LEFT)
-def pinout_txt ():              # pinout info for plain text
-    p1, p2 = [], []
-    for i in range(len(keys)):
-        if i%2 == 0:
-            p1.append(html.P(keys[i]))
-        else:
-            p2.append(html.P(keys[i]))
-    return p1, p2
-
-### CREATE TWO LISTS OF DICTIONARIES (RIGHT AND LEFT CHECKBOXES)
-def checkbox_list ():           # pinout info for check boxes
-    gpionum = gpio_nums()
-    c1d = []
-    c2d = []
-    for i in range(40):
-        pinnum = i+1
-        if i%2 == 0:
-            c1d.append({'label':'','value':f'{pinnum}:{gpionum[i]}'})
-        else:
-            c2d.append({'label':'','value':f'{pinnum}:{gpionum[i]}'})
-    return c1d, c2d
-
-
-c1, c2 = checkbox_list()        # list of dictionaries for checkboxes (blank txt output)
-p1, p2 = pinout_txt()           # right and left sides of pinout txt
 ############################################    ---     VARIABLES
-pinout_top = 130
-pinout_left = 150
+pinout_top = 150
+pinout_left = 180
+
+# list of pins to auto check -- on/lvl 1, output, or not gpio
+l_onpins = pin_auto_set()[0]
+r_onpins = pin_auto_set()[1]
+
+l_inpins = pin_auto_set()[2]
+r_inpins = pin_auto_set()[3]
+
+# list of pin states to display
+left_state = pin_state('left')
+right_state = pin_state('right')
 ############################################    ---     WEBSERVER
 
 ### WEBSERVER
@@ -149,83 +50,142 @@ app.layout = html.Div([                     # show stuff on webs server
     html.H2("RPI 4 GPIO stuff"),    # header
     html.Br(), html.Br(), html.Br(),
 
+## HEADERS
     html.Div(       
         html.H3('pinout'),style={
-            'top':pinout_top-60, 'left':pinout_left+90, 
+            'top':pinout_top-100, 'left':pinout_left+90, 
             'position':'absolute', 'textAlign':'center'}
         ),
-    html.Div(
-        html.P('On/Off'),style={
-            'top':pinout_top-30, 'left':pinout_left+90, 
-            'position':'absolute', 'textAlign':'center'}
+    html.Div(                       # on/off
+        html.P('on/off'),style={
+            'top':pinout_top-30, 'left':pinout_left+100, 
+            'position':'absolute', 'textAlign':'center',
+            #'writing-mode': 'vertical-lr', 'text-orientation': 'upright',
+            'letter-spacing': '-0.5px'}
+        ),
+    html.Div(                       # left input
+        html.P('output'),style={
+            'top':pinout_top-40, 'left':pinout_left+60, 
+            'position':'absolute', 'textAlign':'center',
+            #'writing-mode': 'vertical-lr', 'text-orientation': 'upright',
+            'letter-spacing': '-0.5px'}
     ),
+    html.Div(                       # right input
+        html.P('output'),style={
+            'top':pinout_top-40, 'left':pinout_left+140, 
+            'position':'absolute', 'textAlign':'center',
+            #'writing-mode': 'vertical-lr', 'text-orientation': 'upright',
+            'letter-spacing': '-0.5px'}
+    ),
+
+##  PINOUT 
     html.Div(           # pinout left side (odd numbers)
-        p1, style={
+        left_disp, style={
             'top':pinout_top-9, 
-            'left':pinout_left+15, 
+            'left':pinout_left-5, 
             'position':'absolute', 
             'textAlign':'right', 
             'line-height':4}
             ),
     html.Div(           # pinout right side (even numbers)
-        p2, style={
+        right_disp, style={
             'top':pinout_top-9, 
-            'left':pinout_left+140, 
+            'left':pinout_left+160, 
             'position':'absolute', 
             'line-height':4}
             ),
+    
+    html.Div(           # pin state left side (odd numbers)
+        left_state, style={
+            'top':pinout_top+7, 
+            'left':pinout_left-115, 
+            'position':'absolute', 
+            'textAlign':'right', 
+            'line-height':4,
+            'color': 'red'}
+            ),
+    html.Div(           # pin state right side (even numbers)
+        right_state, style={
+            'top':pinout_top+39, 
+            'left':pinout_left+250, 
+            'position':'absolute', 
+            'line-height':4,
+            'color': 'red'}
+            ),
 
-
+## ON / OFF
     html.Div([          # pinout left side (odd dumbers)
         dcc.Checklist(
-            c1, inline=False, id='c1keys', 
+            options=left_checks, value=l_onpins, inline=False, id='left_onoff_checks', 
             labelStyle = dict(display='block')      # not inline
         ),
-        html.Div(id='info_out1')
+        html.Div(id='left_onoff')
     ], style={'top':pinout_top, 'left':pinout_left+100, 'position':'absolute', 'textAlign':'right'}),
 
     html.Div([          # pinout right side (even numbers)
         dcc.Checklist(
-            c2, inline=False, id='c2keys',
+            options=right_checks, value=r_onpins, inline=False, id='right_onoff_checks',
             labelStyle = dict(display='block')      # not inline
         ),
-        html.Div(id='info_out2')
-    ], style={'top':pinout_top, 'left':pinout_left+120, 'position':'absolute'})
+        html.Div(id='right_onoff')
+    ], style={'top':pinout_top, 'left':pinout_left+120, 'position':'absolute'}),
+
+
+## INPUT / OUTPUT
+    html.Div([          # pinout left side (odd dumbers)
+        dcc.Checklist(
+            options=left_checks, value=l_inpins, inline=False, id='left_inout_checks', 
+            labelStyle = dict(display='block')      # not inline
+        ),
+        html.Div(id='left_inout')
+    ], style={'top':pinout_top, 'left':pinout_left+80, 'position':'absolute', 'textAlign':'right'}),
+
+    html.Div([          # pinout right side (even numbers)
+        dcc.Checklist(
+            options=right_checks, value=r_inpins, inline=False, id='right_inout_checks',
+            labelStyle = dict(display='block')      # not inline
+        ),
+        html.Div(id='right_inout')
+    ], style={'top':pinout_top, 'left':pinout_left+140, 'position':'absolute'})
 ])
 
 
 
 ### CALLBACKS
-last_leftcall = []
-last_rightcall = []
+# on / off
 @app.callback(
-    Output(component_id='info_out1', component_property='children'),
-    Input(component_id='c1keys', component_property='value')
+    Output(component_id='left_onoff', component_property='children'),
+    Input(component_id='left_onoff_checks', component_property='value')
 )
-def left_side (input_value):
-    
-    for i in range(len(input_value)):
-        pinnum = input_value[i][input_value[i].index(':')+1:]   # get gpio number
-        lvl, func = pininfo_get(pinnum)                         # get lvl and func
+def left_onoff (input_value):
+    set_lvl(input_value, 'left')
 
-        if pinnum in GPIO_PINS:
-
-        print(f"pin {pinnum}, {lvl} {func}")
-        last_callback = input_value
-    return 
-
-@app.callback(  
-    Output(component_id='info_out2', component_property='children'),
-    Input(component_id='c2keys', component_property='value')
+@app.callback(
+    Output(component_id='right_onoff', component_property='children'),
+    Input(component_id='right_onoff_checks', component_property='value')
 )
-def right_side (input_value):
-    
-    return 
+def right_onoff (input_value):
+    set_lvl(input_value, 'right')
 
+
+
+# input / output
+@app.callback(
+    Output(component_id='left_inout', component_property='children'),
+    Input(component_id='left_inout_checks', component_property='value')
+)
+def left_inout (input_value):
+    set_func(input_value, 'left')
+
+@app.callback(
+    Output(component_id='right_inout', component_property='children'),
+    Input(component_id='right_inout_checks', component_property='value')
+)
+def right_inout (input_value):
+    set_func(input_value, 'right')
 
 ############################################    ---     MAIN
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT, host=HOST)
-
 
