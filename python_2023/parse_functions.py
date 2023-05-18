1fa6fef4841140b3b6d14f4b920ac5a7e6e80c0d
elys33ve <elys33ve@gmail.com>
2023-05-18 13:09:48 -0400

    fixing last things

--- python_2023/parse_functions.py
+++ python_2023/parse_functions.py
@@ -52,19 +52,18 @@ class Test (object):
         # pcmout_bert: total bits / total errs
         total_bits, total_errs = 0, 0
         for i in range(len(self.pcmout_bert[ch])):
-            total_bits += int(self.pcmout_bert[ch][i][0])
-            total_errs += int(self.pcmout_bert[ch][i][1])
+            total_bits += int(self.pcmout_bert[ch][i]["bits"])
+            total_errs += int(self.pcmout_bert[ch][i]["errs"])
         pcmout = [str(total_bits), str(total_errs)]
 
         # linetest
-        
-
+        if len(self.linetest_in) == 0: return { "pcmout_bert":pcmout }  # if no linetest
         total_bits, total_errs = 0, 0
         sync_fail, polarity_changes = 0, []
         for i in range(len(self.linetest_in[ch])):
             # bits and errs
-            total_bits += int(self.linetest_in[ch][i][0])
-            total_errs += int(self.linetest_in[ch][i][1])
+            total_bits += int(self.linetest_in[ch][i]["bits"])
+            total_errs += int(self.linetest_in[ch][i]["errs"])
             # sync
             if self.linetest_in[ch][i][2] != "sync":
                 sync_fail += 1
@@ -80,3 +79,144 @@ class Test (object):
 
         
 
+
+class Parameters (object):
+    def __init__ (self, test_name="", auto_pol="", data_pol="", rate="", other=""):
+        self.test_name = test_name
+        self.auto_polarity = auto_pol
+        self.data_polarity = data_pol   
+        self.rate = rate
+        self.other = other
+
+
+
+
+
+class Channel (object):
+    def __init__ (self, n, r):
+        self.channel_number = n
+        self.rate = r
+        
+        self.bits = []
+        self.errs = []
+        self.pcmin = []
+        self.changes = []
+
+        self.l_bits = []
+        self.l_errs = []
+        self.sync = []
+        self.inv = []
+        self.l_pcmin = []
+        self.l_changes = []
+
+    def empty(self, x):
+        if len(x) == 0: return True
+        else: return False
+
+    def print_pcmout(self):
+        print("bits: " + str(self.bits))
+        print("errs: " + str(self.errs))
+        print("pcmin: " + str(self.pcmin))
+        print("changes: " + str(self.changes))
+
+    def print_linetest(self):
+        print("bits: " + str(self.l_bits))
+        print("errs: " + str(self.l_errs))
+        print("sync: " + str(self.sync))
+        print("inv: " + str(self.inv))
+        print("pcmin: " + str(self.l_pcmin))
+        print("changes: " + str(self.l_changes))
+    
+    def print_some(self):
+        print(f"ch{self.channel_number}")
+        print(f"\t({self.bits[0]}/{self.errs[0]})")
+        # sync
+        sync = "all sync"
+        if len(self.sync) == 0: sync = "--"
+        for i in range(len(self.sync)):
+            if self.sync[i] != "sync": sync = "a fuck up"
+        # bits/errs
+        if len(self.l_bits) == 0 or len(self.l_errs) == 0: bits, errs = '--', '--'
+        else: bits, errs = self.bits[0], self.errs[0]
+        print(f"\t({bits}/{errs}, {sync})")
+
+    def write_out(self, file):
+        file.write(f"\tch{self.channel_number}\n")
+
+        # pcmout bits / errs
+        bits, errs = '--', '--'
+        if not(self.empty(self.bits) or self.empty(self.errs)):
+            # get sums
+            bits, errs = 0, 0
+            for i in range(len(self.bits)):
+                bits += int(self.bits[i])
+                errs += int(self.errs[i])
+        file.write(f"\t\tpcmout_bert: {bits} bits / {errs} errs\n")
+
+        # linetest bits / errs
+        bits, errs = '--', '--'
+        if not(self.empty(self.l_bits) or self.empty(self.l_errs)):
+            # get sums
+            bits, errs = 0, 0
+            for i in range(len(self.l_bits)):
+                bits += int(self.l_bits[i])
+                errs += int(self.l_errs[i])
+
+        # linetest sync
+        sync = '--'
+        if not(self.empty(self.sync)):
+            sync = "all sync"
+            for i in range(len(self.sync)):
+                if self.sync[i] != "sync":
+                    sync = "weewooweewoo not sync"
+        file.write(f"\t\tlineteset_in: {bits} bits / {errs} errs, {sync}\n")
+
+    def check_pcmins(self, polarities):
+        fail = False
+        # pcmin (pcmout)
+        if len(self.pcmin) > 0:
+            for i in range(len(self.pcmin)):
+                # pass: polarity=1, falling edge
+                if "falling" in self.pcmin[i] and polarities[i] != 1:
+                    fail = True
+                    break
+                # pass: polarity=0, rising edge
+                if "rising" in self.pcmin[i] and polarities[i] != 0:
+                    fail = True
+                    break
+        
+        # pcmin (linetest)
+        if len(self.l_pcmin) > 0:
+            for i in range(len(self.l_pcmin)):
+                # pass: polarity=1, falling edge
+                if "falling" in self.l_pcmin[i] and polarities[i] != 1:
+                    fail = True
+                    break
+                # pass: polarity=0, rising edge
+                if "rising" in self.l_pcmin[i] and polarities[i] != 0:
+                    fail = True
+                    break
+        return fail
+
+
+
+
+class Test (object):
+    def __init__ (self, data_rate, channels):
+        self.data_rate = data_rate
+        self.channels = channels
+        self.data_polarity = -1
+        self.polarities = []
+    
+    def pol_test(self, polarities=[]):
+        if len(polarities) <= 0: polarities = self.polarities
+        if len(polarities) > 0:
+            fails = []
+            for i in range(len(self.channels)):
+                fails.append(self.channels[i].check_pcmins(polarities))
+        return fails
+    
+    def inv_test(self):
+        return
+                
+    
